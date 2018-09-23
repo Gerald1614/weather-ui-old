6a8473a2a79271de8472a88caa361f5ff1133c8d
Gerald Michelant <gmichelant@Gerald-Michelants-MacBook-Pro.local>
2018-09-23 00:16:20 -0400

    socket.io working

---
+++ src/components/Weather.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>{{TempData}}</p>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+      TempData: "toto"
+    }
+  },
+  mounted () {
+    console.log('coucou')
+    this.$options.sockets.sensorData = (data) => {
+      console.log(data)
+      this.TempData = data
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import socketio from 'socket.io-client'
+import VueSocketio from 'vue-socket.io'
+const SocketInstance = socketio('http://192.168.2.14:3000')
+Vue.use(VueSocketio, SocketInstance)
 
 Vue.config.productionTip = false
 
